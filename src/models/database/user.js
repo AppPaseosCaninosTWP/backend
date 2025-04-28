99d8e8f0c99fa06835fff7b3f55f82df75334e37
frnnd <fdcg.contacto1@gmail.com>
2025-04-27 23:37:12 -0400

    feat: modelo del user

---
+++ src/models/database/user.js
@@ -0,0 +1,44 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('./database/sequelize');
+
+const User = sequelize.define('user', {
+  user_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  is_enable: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true,
+  },
+  ticket: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+  role_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'roles',
+      key: 'role_id',
+    },
+    allowNull: false,
+  }
+}, {
+  tableName: 'user',
+  timestamps: false
+});
+
+module.exports = User;
