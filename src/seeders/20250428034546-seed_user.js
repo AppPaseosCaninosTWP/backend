4223fe61935582813c60076d50d562c3500f3aeb
frnnd <fdcg.contacto1@gmail.com>
2025-04-27 23:49:22 -0400

    feat: seeder de usuarios iniciales del sistema

---
+++ src/seeders/20250428034546-seed_user.js
@@ -0,0 +1,46 @@
+"use strict";
+
+const bcrypt = require("bcryptjs");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const admin_password = await bcrypt.hash("admin123", 10);
+    const walker_password = await bcrypt.hash("walker123", 10);
+    const client_password = await bcrypt.hash("client123", 10);
+
+    await queryInterface.bulkInsert(
+      "user",
+      [
+        {
+          email: "admin@example.com",
+          phone: "999999999",
+          password: admin_password,
+          is_enable: true,
+          ticket: false,
+          role_id: 1,
+        },
+        {
+          email: "walker@example.com",
+          phone: "999999999",
+          password: walker_password,
+          is_enable: true,
+          ticket: true,
+          role_id: 2,
+        },
+        {
+          email: "client@example.com",
+          phone: "999999999",
+          password: client_password,
+          is_enable: true,
+          ticket: true,
+          role_id: 3,
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete("user", null, {});
+  },
+};
