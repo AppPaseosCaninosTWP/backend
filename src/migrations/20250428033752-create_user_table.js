bf439f0e74b8c0cccb79db7d515ed942a779ee33
frnnd <fdcg.contacto1@gmail.com>
2025-04-27 23:38:49 -0400

    feat: migración del user

---
+++ src/migrations/20250428033752-create_user_table.js
@@ -0,0 +1,48 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("user", {
+      user_id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      phone: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      is_enable: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true,
+      },
+      ticket: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+      role_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "role",
+          key: "role_id",
+        },
+        allowNull: false,
+        onUpdate: "CASCADE",
+        onDelete: "SET NULL",
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("user");
+  },
+};
