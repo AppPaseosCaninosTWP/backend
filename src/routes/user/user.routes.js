414d284643ce6a2d33c69f62ca602542b90e9f83
frnnd <fdcg.contacto1@gmail.com>
2025-04-29 01:17:24 -0400

    feat: ruta para request_reset_code

--- src/routes/user/user.routes.js
+++ src/routes/user/user.routes.js
@@ -2,7 +2,8 @@ const { Router } = require("express");
 const {
   get_users,
   get_user_by_id,
-  update_is_enable
+  update_is_enable,
+  request_reset_code
 } = require("../../controllers/user_controller");
 const validate_jwt = require("../../middlewares/validate_jwt");
 const allow_roles = require("../../middlewares/allow_roles");
@@ -13,5 +14,8 @@ router.use(validate_jwt);
 router.get("/", allow_roles(1), get_users);
 router.get("/:id", allow_roles(1), get_user_by_id);
 router.put("/:id", allow_roles(1), update_is_enable);
+
+router.post("/request_reset_code", request_reset_code);
+
 
 module.exports = router;
