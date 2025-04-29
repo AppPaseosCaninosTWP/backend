fa88d854aa9d16bcde61f505b444df47266fb1fb
frnnd <fdcg.contacto1@gmail.com>
2025-04-28 23:47:26 -0400

    feat: ruta para update_is_enable y actualizacion en el acceso a rutas por role

--- src/routes/user/user.routes.js
+++ src/routes/user/user.routes.js
@@ -2,11 +2,16 @@ const { Router } = require("express");
 const {
   get_users,
   get_user_by_id,
+  update_is_enable
 } = require("../../controllers/user_controller");
+const validate_jwt = require("../../middlewares/validate_jwt");
+const allow_roles = require("../../middlewares/allow_roles");
 
 const router = Router();
+router.use(validate_jwt);
 
-router.get("/", get_users);
-router.get("/:id", get_user_by_id);
+router.get("/", allow_roles(1), get_users);
+router.get("/:id", allow_roles(1), get_user_by_id);
+router.put("/:id", allow_roles(1), update_is_enable);
 
 module.exports = router;
