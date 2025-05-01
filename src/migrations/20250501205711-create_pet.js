9c1ec616e0c8e9a3a9725263ecd7e5f6bb7a3a10
frnnd <fdcg.contacto1@gmail.com>
2025-05-01 16:57:51 -0400

    feat: modelo y migración para entidad mascota

---
+++ src/migrations/20250501205711-create_pet.js
@@ -0,0 +1,59 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("pet", {
+      pet_id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      breed: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      age: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      zone: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+      },
+      comments: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+      },
+      medical_condition: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+      },
+      photo: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      owner_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "user",
+          key: "user_id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable("pet");
+  },
+};
---
+++ src/models/database/pet.js
@@ -0,0 +1,60 @@
+"use strict";
+
+module.exports = (sequelize, DataTypes) => {
+  const pet = sequelize.define(
+    "pet",
+    {
+      pet_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      breed: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      age: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      zone: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      comments: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      medical_condition: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      photo: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      owner_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: "pet",
+      timestamps: false,
+    }
+  );
+
+  pet.associate = (models) => {
+    pet.belongsTo(models.user, { foreignKey: "owner_id", as: "owner" });
+  };
+
+  return pet;
+};
