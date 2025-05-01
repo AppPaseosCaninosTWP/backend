920eb543580c914b998ba7b14072d2d42c96d1f9
frnnd <fdcg.contacto1@gmail.com>
2025-05-01 17:27:24 -0400

    feat: implementacion de modelo y migración para perfil de paseador

---
+++ src/migrations/20250501212553-create_walker_profile.js
@@ -0,0 +1,55 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("walker_profile", {
+      walker_id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        references: {
+          model: "user",
+          key: "user_id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      experience: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      walker_type: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      zone: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      photo: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+      },
+      balance: {
+        type: Sequelize.DECIMAL,
+        defaultValue: 0,
+      },
+      on_review: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true,
+      },
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable("walker_profile");
+  },
+};
---
+++ src/models/database/walker_profile.js
@@ -0,0 +1,59 @@
+"use strict";
+
+module.exports = (sequelize, DataTypes) => {
+  const walker_profile = sequelize.define(
+    "walker_profile",
+    {
+      walker_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      experience: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      walker_type: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      zone: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      photo: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      balance: {
+        type: DataTypes.DECIMAL,
+        defaultValue: 0,
+      },
+      on_review: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+      },
+    },
+    {
+      tableName: "walker_profile",
+      timestamps: false,
+    }
+  );
+
+  walker_profile.associate = (models) => {
+    walker_profile.belongsTo(models.user, {
+      foreignKey: "walker_id",
+      as: "user",
+    });
+  };
+
+  return walker_profile;
+};
