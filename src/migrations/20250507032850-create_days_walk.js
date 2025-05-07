2e98ad0adc13da2a8803f2a9fc900612e46c89c8
frnnd <fdcg.contacto1@gmail.com>
2025-05-07 06:55:03 -0400

    refactor: mejorar definición de tabla days_walk con clave primaria compuesta y restricciones de integridad

--- src/migrations/20250507032850-create_days_walk.js
+++ src/migrations/20250507032850-create_days_walk.js
@@ -5,12 +5,30 @@ module.exports = {
     await queryInterface.createTable("days_walk", {
       walk_id: {
         type: Sequelize.INTEGER,
-        primaryKey: true,
-        references: { model: "walk", key: "walk_id" },
+        allowNull: false,
+        references: {
+          model: "walk",
+          key: "walk_id",
+        },
+        onDelete: "CASCADE",
       },
-      start_date: Sequelize.DATEONLY,
-      start_time: Sequelize.TIME,
-      duration: Sequelize.INTEGER,
+      start_date: {
+        type: Sequelize.DATEONLY,
+        allowNull: false,
+      },
+      start_time: {
+        type: Sequelize.TIME,
+        allowNull: false,
+      },
+      duration: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+    });
+    await queryInterface.addConstraint("days_walk", {
+      fields: ["walk_id", "start_date"],
+      type: "primary key",
+      name: "pk_days_walk",
     });
   },
 
