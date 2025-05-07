4049b62bdf48e6d28978957e0312f30b866a9f42
frnnd <fdcg.contacto1@gmail.com>
2025-05-06 22:50:46 -0400

    feat: seeder de usuarios con perfil de trabajador

---
+++ src/seeders/20250507024527-seed_my_walker_profile.js
@@ -0,0 +1,23 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert("walker_profile", [
+      {
+        walker_id: 2,
+        name: "Felipe González",
+        experience: 2,
+        walker_type: "esporádico",
+        zone: "centro",
+        photo: "felipe.jpg",
+        description: "amante de los animales, especializado en paseos urbanos",
+        balance: 0,
+        on_review: true,
+      },
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete("walker_profile", { walker_id: 5 });
+  },
+};
