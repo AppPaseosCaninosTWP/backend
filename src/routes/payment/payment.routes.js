668f10a2ba1b8968558cc3af5dce8c1cd53e9996
frnnd <fdcg.contacto1@gmail.com>
2025-05-24 18:45:59 -0400

    fix: ajusta los metodos utilizados en las rutas, en base a los estandares de programacion

--- src/routes/payment/payment.routes.js
+++ src/routes/payment/payment.routes.js
@@ -1,11 +1,11 @@
 const { Router } = require("express");
 const {
-  createPayment,
-  processPayment,
-  verifyCommission,
-  getBalance,
-  generateReceipt,
-  paymentHistory,
+  create_payment,
+  process_payment,
+  verify_commission,
+  get_balance,
+  generate_receipt,
+  payment_history,
 } = require("../../controllers/payment_controller");
 
 const validate_jwt = require("../../middlewares/validate_jwt");
@@ -13,12 +13,12 @@ const validate_jwt = require("../../middlewares/validate_jwt");
 const router = Router();
 
 router.use(validate_jwt);
-router.post("/create_payment", createPayment);
-router.post("/process_payment/:payment_id", processPayment);
-router.post("/:payment_id/verify", verifyCommission);
-router.get("/walker/:walker_id/balance", getBalance);
-router.get("/:payment_id/receipt", generateReceipt);
-router.get("/walkers/:walker_id/history", paymentHistory);
+router.post("/create_payment", create_payment);
+router.post("/process_payment/:payment_id", process_payment);
+router.post("/:payment_id/verify", verify_commission);
+router.get("/walker/:walker_id/balance", get_balance);
+router.get("/:payment_id/receipt", generate_receipt);
+router.get("/walkers/:walker_id/history", payment_history);
 
 
 module.exports = router;
