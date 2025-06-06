48d5c8c62c174a3793db9b346682fb2ee83d9ce3
RonaldCR <119820302+ronald516@users.noreply.github.com>
2025-06-06 16:20:47 -0400

    fix(pet_routes): se ajustan los permisos de algunas rutas del pet

--- src/routes/pet/pet.routes.js
+++ src/routes/pet/pet.routes.js
@@ -15,7 +15,7 @@ const router = Router();
 router.use(validate_jwt);
 
 // Permite al cliente registrar una mascota
-router.post("/", allow_roles(1, 2, 3), upload_image.single('photo'), create_pet);
+router.post("/", allow_roles(3), upload_image.single('photo'), create_pet);
 
 // Obtiene las mascotas del sistema
 // si un admin (rol 1) llama a esta ruta, obtiene todas las mascotas sin indicar un owner_id, ve todas las mascotas del sistema.
@@ -23,7 +23,7 @@ router.post("/", allow_roles(1, 2, 3), upload_image.single('photo'), create_pet)
 // si indica un owner_id, obtiene las mascotas de ese dueño.
 // ejemplo: GET /pet?owner_id=3
 // si un cliente (rol 2) llama a esta ruta, obtiene solo sus mascotas.
-router.get("/", allow_roles(1, 2, 3), get_pets);
+router.get("/", allow_roles(1, 3), get_pets);
 // Obtiene una mascota por id
 router.get("/:id", allow_roles(1, 2, 3), get_pet_by_id);
 
