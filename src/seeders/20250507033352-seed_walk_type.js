5b5f10071001e65b126b1b02e4e33f2553cd3d88
RonaldCR <119820302+ronald516@users.noreply.github.com>
2025-06-06 16:18:00 -0400

    feat(walk_type_seeder): se agrega walk_type 3 de prueba

--- src/seeders/20250507033352-seed_walk_type.js
+++ src/seeders/20250507033352-seed_walk_type.js
@@ -4,13 +4,14 @@ module.exports = {
   up: async (queryInterface) => {
     await queryInterface.bulkInsert('walk_type', [
       { walk_type_id: 1, name: 'Fijo' },
-      { walk_type_id: 2, name: 'Esporádico' }
+      { walk_type_id: 2, name: 'Esporádico' },
+      { walk_type_id: 3, name: 'Prueba' }
     ]);
   },
 
   down: async (queryInterface, Sequelize) => {
     await queryInterface.bulkDelete("walk_type", {
-      walk_type_id: { [Sequelize.Op.in]: [1, 2] }
+      walk_type_id: { [Sequelize.Op.in]: [1, 2, 3] }
     });
   }
 };
