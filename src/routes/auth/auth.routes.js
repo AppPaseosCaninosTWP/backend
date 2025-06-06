7721cdfb5f575005364f0c1f3582893d5deda3a9
RonaldCR <119820302+ronald516@users.noreply.github.com>
2025-06-06 17:35:52 -0400

    feat(auth): implementa verificación de teléfono y registro preliminar con envío de SMS

--- src/routes/auth/auth.routes.js
+++ src/routes/auth/auth.routes.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const {
   login_user,
   register_user,
-  logout,
+  verify_phone,
   request_password_reset,
   reset_password,
 } = require("../../controllers/auth_controller");
@@ -14,10 +14,14 @@ const router = Router();
 router.post("/login", login_user);
 // Permite registrarse
 router.post("/register", register_user);
+// 2) Verificar código
+// POST /api/auth/verify-phone
+router.post("/verify_phone", verify_phone);
 // Envia el codigo para restablecer la contraseña al email 
 router.post("/request_password_reset", request_password_reset);
 // Permite restablecer la contraseña
 router.post("/reset_password", reset_password);
+
 
 // Verifica que el JWT es valido
 router.get("/verify", validate_jwt, (req, res) => {
